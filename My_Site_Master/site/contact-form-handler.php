1afca280365ed57ccc0b6fbe81f4f115a9d0fe3b
ImThatGabe <69359088+ImThatGabe@users.noreply.github.com>
2021-01-14 15:40:10 -0500

    Contact Updates

---
+++ My_Site_Master/site/contact-form-handler.php
@@ -0,0 +1,32 @@
+<?php
+  $fname = $_POST['fname'];
+  $lname = $_POST['lname'];
+  $visitoremail = $_POST['email'];
+  $phonenum = $_POST['phoneNumber'];
+  $message = $_POST['mess'];
+
+  $fnamem = $_POST['fnamem'];
+  $lnamem = $_POST['lnamem'];
+  $visitoremailm = $_POST['emailm'];
+  $phonenum = $_POST['phoneNumber'];
+  $messagem = $_POST['messm'];
+  
+  $email_from = 'Gabe@mylinkpad.com';
+  $email_subject = "New Form Submission";
+  $email_body = "Desktop Submission \nFirst Name: $fname.\n".
+    "Last Name: $lname.\n".
+      "Email: $visitoremail.\n".
+        "Phone Number: $phonenum.\n".
+          "Project Details: $message.\n".
+            "Mobile Submission \nFirst Name: $fnamem.\n".
+                "Last Name: $lnamem.\n".
+                    "Email: $visitoremailm.\n".
+                        "Phone Number: $phonenum.\n".
+                            "Project Details: $messagem.\n".
+
+  $to = "gsm525@outlook.com";
+
+  mail($to,$email_subject,$email_body);
+  header("Location: http://mylinkpad.com/Contact.html");
+
+?>
